c23761414d0149fd422b83a6bdb0ee6267ecb2e5
João Victor <156962782+jvictordev7@users.noreply.github.com>
2024-03-20 11:00:49 -0300

    atualizei EX005

---
+++ EX005/script.js
@@ -0,0 +1,15 @@
+var corpo = window.document.body
+
+var p1 = window.document.getElementsByTagName('p')[0]
+
+//var d = window.document.getElementById('msg')
+
+//d.style.background = 'green'
+
+//d.innerText = 'Estou aguardando.....'
+
+var d = window.document.querySelector('div.msg')
+
+d.style.background = 'blue'
+
+//d.innerText = 'Estou aguardando...'
